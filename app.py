db98877bf055987f7033004056a5f21957982335
Daniel Floyd <gleek.dan@gmail.com>
2020-12-11 16:58:54 -0700

    Add todo class for SQLAlchemy and sqlite setup

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    done = db.Column(db.Boolean, nullable=False)
 
 
 @app.route('/')
--- tests/test_basic_setup.py
+++ tests/test_basic_setup.py
@@ -1,6 +1,13 @@
+import os
+
 import requests
 
 
 def test_hello_world():
     response = requests.get('http://localhost:5000')
     assert response.ok
+
+
+def test_sqlite_file_exists():
+    assert os.path.exists('../db.sqlite') is True
+
